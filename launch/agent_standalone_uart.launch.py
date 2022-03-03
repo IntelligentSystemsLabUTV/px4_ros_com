1394868084a5d8d2de8959c7a34ee7d740b788bc
Roberto Masocco <robmasocco@gmail.com>
2022-03-03 17:29:09 +0000

    feat: add launch files for Agent

---
+++ launch/agent_standalone_uart.launch.py
@@ -0,0 +1,28 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+"""Generates a launch description for the microRTPS Agent."""
+def generate_launch_description():
+  ld = LaunchDescription()
+  agent_node = Node(
+    package='px4_ros_com',
+    executable='micrortps_agent',
+    namespace='fmu',
+    shell=True,
+    emulate_tty=True,
+    output='both',
+    log_cmd=True,
+    arguments=[
+      '-n',
+      'fmu', # Everything will be under the "/fmu" namespace
+      '-t',
+      'UART', # UART transport,
+      '-d',
+      '/dev/ttyACM0', # UART device
+      '-b',
+      '921600', # UART device baudrate
+      '--' # ROS 2 options will come after this, so getopt must stop here
+    ]
+  )
+  ld.add_action(agent_node)
+  return ld
---
+++ launch/agent_standalone_udp.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+"""Generates a launch description for the microRTPS Agent."""
+def generate_launch_description():
+  ld = LaunchDescription()
+  agent_node = Node(
+    package='px4_ros_com',
+    executable='micrortps_agent',
+    namespace='fmu',
+    shell=True,
+    emulate_tty=True,
+    output='both',
+    log_cmd=True,
+    arguments=[
+      '-n',
+      'fmu', # Everything will be under the "/fmu" namespace
+      '-t',
+      'UDP', # UDP transport
+      '--' # ROS 2 options will come after this, so getopt must stop here
+    ]
+  )
+  ld.add_action(agent_node)
+  return ld
